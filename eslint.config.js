836f464914215c7954b1c726a7e0d32a94689d90
= <=>
2026-05-02 14:19:59 -0400

    fix what files are visible

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,9 @@ import js from '@eslint/js'
 import prettierConfig from 'eslint-config-prettier'
 
 export default [
-  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    ...js.configs.recommended,
+  },
   prettierConfig,
-  { rules: {} },
 ]
